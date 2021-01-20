3cc0f76d203482b7df89cf3d30f05e72b035c535
khairul anas <mafiakanan@gmail.com>
2021-01-20 10:42:45 +0700

    request api

--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -1,3 +1,5 @@
+import TheMovieDbSource from '../../data/themoviedb-source';
+
 const NowPlaying = {
   async render() {
     return `
@@ -7,6 +9,10 @@ const NowPlaying = {
 
   async afterRender() {
     // Fungsi ini akan dipanggil setelah render()
+    const movies = await TheMovieDbSource.nowPlayingMovies();
+    console.log(movies);
+
+    // TODO: tampilkan movies di dalam DOM
   },
 };
 
