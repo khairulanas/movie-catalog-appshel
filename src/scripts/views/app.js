9d950f57e91ab4f7ccbbcc29f9509c7774ede88d
khairul anas <mafiakanan@gmail.com>
2021-01-20 10:08:45 +0700

    navigation drawer

---
+++ src/scripts/views/app.js
@@ -0,0 +1,25 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+
+/* eslint-disable no-underscore-dangle */
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
+    // kita bisa menginisiasikan komponen lain bila ada
+  }
+}
+
+export default App;
