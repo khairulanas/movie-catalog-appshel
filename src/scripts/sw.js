5d841e6f8db81f5cd71ca52c6c4809236fc83737
khairul anas <mafiakanan@gmail.com>
2021-01-24 06:41:58 +0700

    merapkan cache pada service worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,20 +1,19 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
-self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
+import CacheHelper from './utils/cache-helper';
 
+const { assets } = global.serviceWorkerOption;
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
   // TODO: Caching App Shell Resource
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
-
+  event.waitUntil(CacheHelper.deleteOldCache());
   // TODO: Delete old caches
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
   // TODO: Add/get fetch request to/from caches
 });
